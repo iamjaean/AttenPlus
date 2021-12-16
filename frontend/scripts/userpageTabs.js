68af2ccadc7e172e9ed0a8fc2e19090b79dd98a1
도영 김 <lark.noanswer@gmail.com>
2021-12-16 23:29:14 +0900

    tab 기능 수정

--- frontend/scripts/userpageTabs.js
+++ frontend/scripts/userpageTabs.js
@@ -1,33 +1,19 @@
-let content1 = document.getElementById("content1");
-let content2 = document.getElementById("content2");
-let content3 = document.getElementById("content3");
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
 
-function openProfile() {
-  content1.style.transform = "translateX(0%)";
-  content2.style.transform = "translateX(100%)";
-  content3.style.transform = "translateX(100%)";
-  btn1.style.borderBottom = "3px solid black";
-  btn2.style.borderBottom = "transparent";
-  btn3.style.borderBottom = "transparent";
-}
+    tab.classList.add("active");
+    target.classList.add("active");
+  });
+});
 
-function openJoinedChallenges() {
-  content1.style.transform = "translateX(100%)";
-  content2.style.transform = "translateX(0%)";
-  content3.style.transform = "translateX(100%)";
-  btn1.style.borderBottom = "transparent";
-  btn2.style.borderBottom = "3px solid black";
-  btn3.style.borderBottom = "transparent";
-}
-function openCreatedChallenges() {
-  content1.style.transform = "translateX(100%)";
-  content2.style.transform = "translateX(100%)";
-  content3.style.transform = "translateX(0%)";
-  btn1.style.borderBottom = "transparent";
-  btn2.style.borderBottom = "transparent";
-  btn3.style.borderBottom = "3px solid black";
-}
+//  btn3.style.borderBottom = "3px solid black";
