0ab53503bccd367c967e0b2d42b08c03d4c97094
도영 김 <lark.noanswer@gmail.com>
2021-12-16 13:40:35 +0900

    userpage 기본 틀 작성

---
+++ frontend/scripts/userPageIndex.js
@@ -0,0 +1,12 @@
+import {
+  changeContent,
+  goBackContent,
+  setPasswordInput,
+} from "userInfoCorrection";
+import {
+  openProfile,
+  openJoinedChallenges,
+  openCreatedChallenges,
+} from "userpageTabs";
+
+import { content1, content2, content3, btn1, btn2, btn3 } from "./userpageTabs";
