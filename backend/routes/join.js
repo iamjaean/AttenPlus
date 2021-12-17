4122f300e1f53d72e65e8404d8003e3cb581ec56
sunky97 <hsk11261612@gmail.com>
2021-12-17 18:01:03 +0900

    회원가입 API 등록

---
+++ backend/routes/join.js
@@ -0,0 +1,28 @@
+const { Router } = require("express");
+const asyncHandler = require("../utils/async-handler");
+const { User } = require("../models");
+const hashPassword = require("../utils/hash-password");
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.send("hello");
+});
+
+//회원가입
+router.get(
+  "/register",
+  asyncHandler(async (req, res, next) => {
+    const { email, userName, password } = req.body;
+    const hashedPassword = hashPassword(password);
+    const user = await User.create({
+      email,
+      name: userName,
+      password: hashedPassword,
+    });
+    console.log("신규 유저" + user);
+
+    res.send(user);
+  })
+);
+
+module.exports = router;
