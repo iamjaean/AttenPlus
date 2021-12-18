84e86285543718d20c94b6180afce8eabb54dd52
도영 김 <lark.noanswer@gmail.com>
2021-12-18 21:45:19 +0900

    ejs 기초 설정 및 테스트

--- backend/app.js
+++ backend/app.js
@@ -2,31 +2,28 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
-const userRouter = require("./routes/users");
-const authRouter = require("./routes/auth");
-const passport = require("passport");
-const cookieParser = require("cookie-parser");
-const getUserFromJWT = require("./middlewares/get-user-from-jwt");
-require("./passport")();
+const userRouter = require("./routes/join");
+const userPageRouter = require("./routes/userpage");
+const path = require("path");
 // DB 연결
 mongoose.connect("mongodb://localhost:27017/simple-board");
 mongoose.connection.on("connected", () => {
   console.log("MongoDB Connected");
 });
 
+app.set("view engine", "ejs");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(passport.initialize());
-app.use(cookieParser());
-app.use(getUserFromJWT);
+app.use(express.static(path.join(__dirname, "../frontend/src")));
+app.use(express.static(path.join(__dirname, "../frontend/public")));
 
 app.get("/", (req, res) => {
   res.send("Hello World!!!!");
 });
 
-app.use("/users", userRouter);
-
-app.use("/auth", authRouter);
+app.use("/join", userRouter);
+app.use("/userpage", userPageRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
