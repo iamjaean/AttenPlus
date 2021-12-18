f1ed11c10da38839a1505083738c6d2986781ad6
sunky97 <hsk11261612@gmail.com>
2021-12-18 19:57:24 +0900

    jwt 로그인 구현

--- backend/passport/index.js
+++ backend/passport/index.js
@@ -2,8 +2,12 @@ const passport = require("passport");
 
 const kakao = require("./strategies/kakao");
 const google = require("./strategies/google");
+const local = require("./strategies/local");
+const jwt = require("./strategies/jwt");
 
 module.exports = () => {
   passport.use(google);
   passport.use(kakao);
+  passport.use(local);
+  passport.use(jwt);
 };
