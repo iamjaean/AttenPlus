d223d52a952697c345b19cff8eca859299dfea57
도영 김 <lark.noanswer@gmail.com>
2021-12-20 19:32:05 +0900

    login과 연결

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
-const mainRouter = require('./routes/main');
+const mainRouter = require("./routes/main");
 // const userRouter = require("./routes/users");
 const userPageRouter = require("./routes/userpage");
 const authRouter = require("./routes/auth");
+const signRouter = require("./routes/sign");
 const path = require("path");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
@@ -30,6 +31,7 @@ app.use("/", mainRouter);
 // app.use("/users", userRouter);
 app.use("/auth", authRouter);
 app.use("/userpage", userPageRouter);
+app.use("/sign", signRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
