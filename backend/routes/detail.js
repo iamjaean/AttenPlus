93afbef64294bc496e2e69f0b08edbeefd96c91e
Hyunwoo Noh <rkskek078@gmail.com>
2021-12-21 01:57:44 +0000

    Upload New File

---
+++ backend/routes/detail.js
@@ -0,0 +1,47 @@
+const { Router } = require("express");
+const { Challenge } = require("../models");
+
+const router = Router();
+
+router.get("/:shortId", async(req, res, next) => {
+    const { shortId } = req.params;
+    const challenge = await Challenge.findOne({
+      shortId,
+    });
+    res.render('detailPage_test',{challenge:challenge});
+  });
+
+
+
+router.post("/:shortId/comments", async (req, res, next) => {
+    const { shortId } = req.params;
+    const { content } = req.body;
+    try{
+      await Challenge.updateOne({shortId}, {
+          $push: { comments: {
+              content,
+          }},
+      });
+    }
+    catch(err){
+      next(err)
+    }
+    res.redirect(`/detail/${shortId}`);
+});
+
+router.post("/:shortId/attendance", async (req, res, next) => {
+    const { shortId } = req.params;
+    const { content } = req.body;
+    try{
+      await Challenge.updateOne({shortId}, {
+          $push: { comments: {
+              content,
+          }},
+      });
+    }
+    catch(err){
+      next(err)
+    }
+    res.redirect(`/detail/${shortId}`);
+});
+module.exports = router;
