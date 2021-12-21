d8cb8881e85509dbd03672679a26d02e36311504
Hyunwoo Noh <rkskek078@gmail.com>
2021-12-21 02:02:19 +0000

    Update main.js

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -4,7 +4,8 @@ const { Challenge } = require("../models");
 const router = Router();
 
 router.get("/", async (req, res, next) => {
-  Challenge.find({}, (err, challenges) => {
+   const challenges = Challenge.find({}).sort({createdAt: -1});
+   challenges.find({}, (err, challenges) => {
     if (err) {
       console.log(err);
       res.status(500).send('An error occurred', err);
@@ -14,29 +15,5 @@ router.get("/", async (req, res, next) => {
     }
   });
 });
-router.get("/:shortId", async(req, res, next) => {
-  const { shortId } = req.params;
-  const challenge = await Challenge.findOne({
-    shortId,
-  });
-  res.render('detailPage_test',{challenge:challenge});
-});
-router.post("/:shortId/comments", async(req, res, next) => {
-  const { shortId } = req.params;
-  const { content } = req.body;
-  const author = "작성자";
-  console.log(content);
-  try{
-    await Challenge.updateOne({shortId}, {
-        $push: { comments: {
-            content,
-            author,
-        }},
-    });
-  }
-  catch(err){
-    next(err)
-  }
-  res.redirect("/");
-});
+
 module.exports = router;
