9df5c7e240e56293a805437b548683b7bf9002aa
sunky97 <hsk11261612@gmail.com>
2021-12-21 18:21:37 +0900

    login.ejs 유효성 검사 적용

--- backend/public/js/login.js
+++ backend/public/js/login.js
@@ -1,5 +1,8 @@
 window.onload = function () {
   let inputEmail = document.getElementById("email");
+  let inputPwd = document.getElementById("password");
+  inputEmail.addEventListener("input", checkUserEmail);
+  inputPwd.addEventListener("input", checkUserPwd);
 
   function checkUserEmail() {
     let spanTag = this.parentElement;
@@ -8,13 +11,11 @@ window.onload = function () {
         spanTag.classList.remove("isValid");
         spanTag.nextElementSibling.remove();
       }
-      return;
     } else if (/^\S+@\S+\.\S+$/.test(inputEmail.value)) {
       if (spanTag.classList.contains("isValid")) {
         spanTag.classList.remove("isValid");
         spanTag.nextElementSibling.remove();
       }
-      return;
     } else if (
       !/^\S+@\S+\.\S+$/.test(inputEmail.value) &&
       !spanTag.classList.contains("isValid")
@@ -27,9 +28,38 @@ window.onload = function () {
       warning.appendChild(warningText);
 
       spanTag.parentElement.appendChild(warning);
-      return;
     }
   }
 
-  inputEmail.addEventListener("keyup", checkUserEmail);
+  function checkUserPwd() {
+    let spanTag = this.parentElement;
+
+    if (inputPwd.value.length <= 0) {
+      if (spanTag.classList.contains("isValid")) {
+        spanTag.classList.remove("isValid");
+        spanTag.nextElementSibling.remove();
+      }
+    } else if (
+      /^.{6,20}$/.test(inputPwd.value) &&
+      spanTag.classList.contains("isValid")
+    ) {
+      if (spanTag.classList.contains("isValid")) {
+        spanTag.classList.remove("isValid");
+        spanTag.nextElementSibling.remove();
+      }
+    } else if (
+      !/^.{6,20}$/.test(inputPwd.value) &&
+      !spanTag.classList.contains("isValid")
+    ) {
+      spanTag.classList.add("isValid");
+      let warning = document.createElement("div");
+      warning.classList.add("form_warning");
+      let warningText = document.createTextNode(
+        "비밀번호는 6자 이상, 20자 이하로 입력하세요."
+      );
+      warning.appendChild(warningText);
+
+      spanTag.parentElement.insertBefore(warning, spanTag.nextElementSibling);
+    }
+  }
 };
