64ac0d7d4bac58bb2ffb7b4807eaa80e8efcd1ff
Hyunwoo Noh <rkskek078@gmail.com>
2021-12-22 09:16:48 +0000

    Update create.js

--- backend/routes/create.js
+++ backend/routes/create.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { Challenge, User, JoinChallenge, CreateChallenge, attendenceCheck } = require("../models");
+const { Challenge, User, JoinChallenge,  attendenceCheck } = require("../models/index");
 const multer = require("multer");
 var fs = require("fs");
 var path = require("path");
@@ -17,6 +17,7 @@ const upload = multer({ storage: storage });
 const router = Router();
 
 router.get("/", async (req, res, next) => {
+  
   res.render("createPage");
 });
 
@@ -33,6 +34,7 @@ router.post("/", upload.single("uploaded_file"), async (req, res, next) => {
     startdate,
     enddate,
   } = req.body;
+
   const category_arr = [
     exercise_check,
     life_check,
@@ -40,6 +42,9 @@ router.post("/", upload.single("uploaded_file"), async (req, res, next) => {
     competency_check,
     hobby_check,
   ];
+  const author = await User.findOne({
+    shortId: req.user.shortId,
+  });
   const img = {
     data: fs.readFileSync(
       path.join(__dirname + "/data/uploads/" + req.file.filename)
@@ -61,48 +66,10 @@ router.post("/", upload.single("uploaded_file"), async (req, res, next) => {
       category,
       startdate,
       enddate,
+      author,
     });
     res.redirect("/");
   } catch (err) {
     next(err);
   }
 });
-
-
-router.get("/:shortId", async (req, res, next) => {
-  const { shortId } = req.params;
-  const post = await Challenge.findOne({
-    shortId,
-  });
-
-  res.send(post);
-});
-
-router.post("/:shortId", async (req, res, next) => {
-  const { shortId } = req.params;
-  const { title, description, category, startdate, enddate } = req.body;
-
-  try {
-    await Challenge.updateOne(
-      { shortId },
-      {
-        title,
-        description,
-        category,
-        startdate,
-        enddate,
-      }
-    );
-    res.redirect(`/posts/${shortId}`);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.delete("/:shortId", async (req, res, next) => {
-  const { shortId } = req.params;
-  await Challenge.deleteOne({ shortId });
-  res.send("OK");
-});
-module.exports = router;
-
