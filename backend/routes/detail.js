b7ee30a450e7a628ce598145bd8ec9e1d2a07e3a
lsh9955 <cqe9933@gmail.com>
2021-12-22 15:32:02 +0900

    detailpage 라우터 연결(아직 정확하게 구현된 것은 아님)

--- backend/routes/detail.js
+++ backend/routes/detail.js
@@ -3,45 +3,51 @@ const { Challenge } = require("../models");
 
 const router = Router();
 
-router.get("/:shortId", async(req, res, next) => {
-    const { shortId } = req.params;
-    const challenge = await Challenge.findOne({
-      shortId,
-    });
-    res.render('detailPage_test',{challenge:challenge});
+router.get("/:shortId", async (req, res, next) => {
+  const { shortId } = req.params;
+  const challenge = await Challenge.findOne({
+    shortId,
   });
-
-
+  res.render("detailPage", { challenge: challenge });
+});
 
 router.post("/:shortId/comments", async (req, res, next) => {
-    const { shortId } = req.params;
-    const { content } = req.body;
-    try{
-      await Challenge.updateOne({shortId}, {
-          $push: { comments: {
-              content,
-          }},
-      });
-    }
-    catch(err){
-      next(err)
-    }
-    res.redirect(`/detail/${shortId}`);
+  const { shortId } = req.params;
+  const { content } = req.body;
+  try {
+    await Challenge.updateOne(
+      { shortId },
+      {
+        $push: {
+          comments: {
+            content,
+          },
+        },
+      }
+    );
+  } catch (err) {
+    next(err);
+  }
+  res.redirect(`/detail/${shortId}`);
 });
 
 router.post("/:shortId/attendance", async (req, res, next) => {
-    const { shortId } = req.params;
-    const { content } = req.body;
-    try{
-      await Challenge.updateOne({shortId}, {
-          $push: { comments: {
-              content,
-          }},
-      });
-    }
-    catch(err){
-      next(err)
-    }
-    res.redirect(`/detail/${shortId}`);
+  const { shortId } = req.params;
+  const { content } = req.body;
+  try {
+    await Challenge.updateOne(
+      { shortId },
+      {
+        $push: {
+          comments: {
+            content,
+          },
+        },
+      }
+    );
+  } catch (err) {
+    next(err);
+  }
+  res.redirect(`/detail/${shortId}`);
 });
 module.exports = router;
