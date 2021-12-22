1014299fde0979b1eeba96f730e15ac81c79cfd3
sunky97 <hsk11261612@gmail.com>
2021-12-22 14:30:29 +0900

    port 변경

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/main");
 const userPageRouter = require("./routes/userpage");
