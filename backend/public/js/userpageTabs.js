ae4caf2c953810083a0192ddc86fbedfe3a786af
도영 김 <lark.noanswer@gmail.com>
2021-12-23 15:56:30 +0900

    loading spinner 추가

--- backend/public/js/userpageTabs.js
+++ backend/public/js/userpageTabs.js
@@ -4,6 +4,8 @@ const tabContents = document.querySelectorAll("[data-tab-content]");
 const joinedChallenges = document.querySelector(".joinedChallenge");
 const createdChallenges = document.querySelector(".createdChallenge");
 
+const wrapper = document.getElementById("loader-wrapper");
+
 let limit = 16;
 let pageJoined = 0;
 let pageCreated = 0;
@@ -27,10 +29,12 @@ tabs.forEach((tab) => {
     if (target.id == "content2") {
       createdChallenges.innerHTML = "";
       pageCreated = 0;
+      wrapper.innerHTML = `<div class="loader">Loading...</div>`;
       showJoinedChallenges();
     } else if (target.id == "content3") {
       joinedChallenges.innerHTML = "";
       pageJoined = 0;
+      wrapper.innerHTML = `<div class="loader">Loading...</div>`;
       showCreatedChallenges();
     } else {
       createdChallenges.innerHTML = "";
@@ -53,22 +57,35 @@ async function getJoinedChallenges() {
   return data;
 }
 
+// 이미지 렌더링을 위한 helper function
+
+function Uint8ToString(u8array) {
+  var unitLength = 0x8000; // 16 bit을 unit length로 설정
+  var c = [];
+  // uint8array를 unitLength의 크기로 잘라 string으로 변환한뒤 합쳐준다.
+  for (var i = 0; i < u8array.length; i += unitLength) {
+    c.push(
+      String.fromCharCode.apply(null, u8array.subarray(i, i + unitLength))
+    );
+  }
+  return c.join("");
+}
+
 // 참여한 챌린지들을 페이지에 렌더
 
 async function showJoinedChallenges() {
   const challenges = await getJoinedChallenges();
 
   challenges.forEach((challenge) => {
-    const imgBuffer = challenge.img.data.data;
-    const base64String = window.btoa(
-      String.fromCharCode.apply(null, new Uint8Array(imgBuffer))
-    );
+    const imgBuffer = new Uint8Array(challenge.img.data.data);
+    const base64String = window.btoa(Uint8ToString(imgBuffer));
     joinedChallenges.innerHTML += `
                     <article class="card">
-                      <div class="img-wrapper">
+                      <div class="user-img-wrapper">
                         <a href="">
-                            <img loading ="lazy" src= "data:img/${challenge.img.contentType};base64,
-                     ${base64String}" alt="참여한 챌린지">
+                            <img
+              src="data:image/${challenge.img.contentType};base64,${base64String}"
+              alt="참여한 챌린지"/>
                         </a>
                       </div>
                       <div>
@@ -79,8 +96,9 @@ async function showJoinedChallenges() {
                         <a href="" class="challenge-title">${challenge.title}</a>
                       </div>
                     </article>
-                `;
+    `;
   });
+  wrapper.innerHTML = "";
 }
 
 // 생성한 챌린지들 데이터를 받음
@@ -102,31 +120,26 @@ async function getCreatedChallenges() {
 async function showCreatedChallenges() {
   const challenges = await getCreatedChallenges();
   challenges.forEach((challenge) => {
-    const imgBuffer = challenge.img.data.data;
-    const base64String = window.btoa(
-      String.fromCharCode.apply(null, new Uint8Array(imgBuffer))
-    );
     createdChallenges.innerHTML += `
                     <article class="card">
-                      <div class="img-wrapper">
+                      <div class="user-img-wrapper">
                         <a href="">
                             <img
-              src="data:image/${challenge.img.contentType};base64,${base64String}"
-              alt="/"
-             
-            />
+              src="data:image/${challenge.img.contentType};base64,${challenge.img.data}"
+              alt="생성된 챌린지"/>
                         </a>
                       </div>
                       <div>
                         <div class="detail-info">
                                 <p class="challenge-category">${challenge.category}</p>
-                                <p class="challenge-maker">${challenge.author.name}</p>
+                                <p class="challenge-maker">${challenge.name}</p>
                         </div>
                         <a href="" class="challenge-title">${challenge.title}</a>
                       </div>
                     </article>
     `;
   });
+  wrapper.innerHTML = "";
 }
 
 // infinite scroll 기능구현을 위한 eventlistener
