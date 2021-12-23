8c7e2c04706f167a4ff385d19f65d2908ab2b4be
Hyunwoo Noh <rkskek078@gmail.com>
2021-12-23 12:56:45 +0000

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 const UserSchema = require("./schemas/user");
-const JoinChallengeeSchema = require('./schemas/joinChallenge');
 const ChallengeSchema = require("./schemas/challenge");
 const attendanceCheckSchema = require('./schemas/attendanceCheck');
 
 exports.Challenge = mongoose.model("Challenge", ChallengeSchema);
 exports.User = mongoose.model("User", UserSchema);
-exports.JoinChallenge = mongoose.model("JoinChallenge", JoinChallengeeSchema);
 exports.attendanceCheck = mongoose.model("attendanceCheck", attendanceCheckSchema);
