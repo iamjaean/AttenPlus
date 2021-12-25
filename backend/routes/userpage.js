60daf42e8d282c709a692ca83385d62c86f1892e
도영 김 <lark.noanswer@gmail.com>
2021-12-25 19:50:15 +0900

    comment 추가

--- backend/routes/userpage.js
+++ backend/routes/userpage.js
@@ -40,7 +40,7 @@ function checkFileType(file, cb) {
   }
 }
 
-// user 페이지 접속시 shortId에 따라 맞는 페이지로 라우팅.
+// user 페이지 접속시 shortId에 따라 각 유저에 맞는 페이지로 라우팅.
 
 router.get(
   "/",
@@ -54,7 +54,7 @@ router.get(
   })
 );
 
-// 접속 유저에 대한 기본 정보
+// 접속 유저에 대한 기본 정보 (이름과 소개)
 
 router.get(
   "/:shortId/userinfo",
@@ -94,7 +94,7 @@ router.post(
   })
 );
 
-// image 업로드
+// profile image 업로드
 
 router.post(
   "/:shortId/upload",
@@ -102,11 +102,13 @@ router.post(
   asyncHandler(async (req, res) => {
     const { shortId } = req.params;
 
+    // image를 업로드하지 않았을시
     if (typeof req.file == "undefined") {
       res.redirect(`/user/${shortId}`);
       return;
     }
 
+    //image는 각 유저의 데이터베이스에 직접 저장한다.
     await User.findOneAndUpdate(
       { shortId },
       {
@@ -148,6 +150,7 @@ router.post(
     const { currentPassword, newPassword, confirmNewPassword } = req.body;
     const author = await User.findOne({ shortId });
 
+    // 비밀번호 입력에 대한 오류 처리
     if (currentPassword) {
       if (hashPassword(currentPassword) !== author.password) {
         res.send(
@@ -164,6 +167,7 @@ router.post(
       }
     }
 
+    // 입력이 올바르면 업데이트 실행
     await User.findOneAndUpdate(
       { shortId },
       {
@@ -189,6 +193,8 @@ router.get(
     const author = await User.findOne({
       shortId,
     });
+
+    // 모든 생성한 챌린지를 다 내려주는 것이 아닌, url query 값에 맞춰 부분적으로 끊어서 반환
     const challenges = Challenge.find({ author: author })
       .sort({ updatedAt: -1 })
       .populate("author", "name")
@@ -201,6 +207,7 @@ router.get(
       } else {
         const createdChallenges = [];
 
+        // 보안을 위해 모든 정보를 내리진 않고, 사용자에게 보여줄 정보만 골라낸 뒤 전송
         challenges.forEach((challenge) => {
           let b64 = Buffer.from(challenge.img.data).toString("base64");
           let challengeUrl = `http://elice-kdt-sw-1st-vm04.koreacentral.cloudapp.azure.com/detail/${challenge.shortId}`;
@@ -236,6 +243,7 @@ router.get(
     const user = await User.findOne({
       shortId,
     });
+    // 모든 참여한 챌린지를 다 내려주는 것이 아닌, url query 값에 맞춰 부분적으로 끊어서 반환
     const challenges = Challenge.find({ joinusers: { $in: user } })
       .sort({ updatedAt: -1 })
       .populate("author", "name")
@@ -249,6 +257,7 @@ router.get(
       } else {
         const joinedChallenges = [];
 
+        // 보안을 위해 모든 정보를 내리진 않고, 사용자에게 보여줄 정보만 골라낸 뒤 전송
         challenges.forEach((challenge) => {
           let b64 = Buffer.from(challenge.img.data).toString("base64");
           let challengeUrl = `http://elice-kdt-sw-1st-vm04.koreacentral.cloudapp.azure.com/detail/${challenge.shortId}`;
