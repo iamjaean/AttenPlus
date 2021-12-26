e3075d1c09846ab25cc6734518d464ca60a23fd7
sunky97 <hsk11261612@gmail.com>
2021-12-26 16:46:01 +0900

    Update category.ejs

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -5,7 +5,10 @@ const router = Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const challenges = await Challenge.find({}).sort({ createdAt: -1 }).populate("author").populate("joinusers");
+    const challenges = await Challenge.find({})
+      .sort({ createdAt: -1 })
+      .populate("author")
+      .populate("joinusers");
 
     let user = "";
     if (req.cookies.token) {
@@ -15,6 +18,37 @@ router.get("/", async (req, res, next) => {
     }
     res.render("main", { challenges: challenges, user: user });
   } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/board", async (req, res, next) => {
+  const category = ["운동", "생활", "정서", "역량", "취미"];
+  try {
+    const param = req.query.category;
+    if (category.includes(param)) {
+      const challenges = await Challenge.find({
+        category: param,
+      })
+        .sort({ createdAt: -1 })
+        .populate("author")
+        .populate("joinusers");
+      let user = "";
+      if (req.cookies.token) {
+        user = await User.findOne({
+          shortId: req.user.shortId,
+        });
+      }
+      res.render("category", {
+        title: param,
+        challenges: challenges,
+        user: user,
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.log(err);
     next(err);
   }
 });
